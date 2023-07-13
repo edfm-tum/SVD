a12752cd3c78ef18dfdd21e320ba1df4e0379c5b
SVDmodel <werner.rammer@tum.de>
2023-07-13 09:16:19 +0200

    trying to implement GPU device selection
    Not functional yet.

--- SVDModel/Predictor/dnn.cpp
+++ SVDModel/Predictor/dnn.cpp
@@ -150,6 +150,8 @@ bool DNN::setupDNN(size_t aindex)
 
     mNResTimeCls = settings.valueUInt("dnn.restime.N");
 
+    std::string selected_gpu = settings.valueString("dnn.selectedGPU");
+
 
     lg->info("DNN file: '{}'", file);
 
@@ -195,9 +197,10 @@ bool DNN::setupDNN(size_t aindex)
     //auto device_count = config->device_count();
     //lg->debug("device-count GPU: {}, CPU: {}", device_count["GPU"], device_count["CPU"]);
 
+    if (!selected_gpu.empty())
+        setenv("CUDA_VISIBLE_DEVICES", selected_gpu.c_str(), 1);
 
-    setenv("CUDA_VISIBLE_DEVICES", to_string(aindex).c_str(), 1);
-    lg->debug("CUDA_VISBLE_DEVICES = {}", getenv("CUDA_VISIBLE_DEVICES"));
+    lg->debug("CUDA_VISBLE_DEVICES = {} (dnn.selectedGPU was: {})", getenv("CUDA_VISIBLE_DEVICES"), selected_gpu);
 
     //if (aindex == 1) {
         //std::string devices = "2";
